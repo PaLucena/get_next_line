3df1d827a03e0728dec42248b12017806a5fffe0
palucena <palucena@student.42malaga.com>
2023-05-02 13:00:35 +0200

    Added functions

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/02 12:42:01 by palucena          #+#    #+#             */
+/*   Updated: 2023/05/02 12:57:11 by palucena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+char    *get_next_line(int fd);
+
+#endif

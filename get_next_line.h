449919cdc69b50550fece4a66d5491d0257d6a5a
palucena <palucena@student.42malaga.com>
2023-05-02 14:16:05 +0200

    1 function

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 12:42:01 by palucena          #+#    #+#             */
-/*   Updated: 2023/05/02 12:57:11 by palucena         ###   ########.fr       */
+/*   Updated: 2023/05/02 13:17:46 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
+# include <unistd.h>
 # include <stdlib.h>
 
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 
 #endif
